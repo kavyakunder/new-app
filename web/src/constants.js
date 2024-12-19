2e04ecff31a612b7b6248d08ff792be6209913af
kavyakunder10 <kavyakunder7082@gmail.com>
2024-12-19 10:26:08 +0530

    correct the import

--- web/src/constants.js
+++ web/src/constants.js
@@ -1,5 +1,6 @@
 // Make sure to import the helper functions from the `ssr` entry point.
-import { entryPointUriPathToPermissionKeys } from '@commercetools-frontend/application-shell/ssr';
+// import { entryPointUriPathToPermissionKeys } from '@commercetools-frontend/application-shell/ssr';
+import { entryPointUriPathToPermissionKeys } from '@commercetools-frontend/application-shell/ssr/dist/commercetools-frontend-application-shell-ssr.cjs.js';
 
 export const entryPointUriPath = 'chatbot';
 
